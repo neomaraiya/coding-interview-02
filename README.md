f515d5ec2be3c07806f041ddbc7c372384f4a26b
neomaraiya <neomaraiya@outlook.com>
2021-05-14 19:38:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# coding-interview-02
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/coding-interview-02)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+// 1. Call API on page load -https://reqres.in/api/users?page=1
+// 2. Display data in string format
+// 3. Create a button to fetch next page data.
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
